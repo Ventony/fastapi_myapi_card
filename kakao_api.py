2681b943944a597f7a2e997154efb3a5a4d498b8
Ventony <dean5509@naver.com>
2024-04-16 12:43:50 +0900

    access_code_to_my_kakao_send

--- kakao_api.py
+++ kakao_api.py
@@ -14,15 +14,34 @@
 # 2. 인증코드를 사용해서 토큰 발급 받기
 
 import requests
+import json
 
 url = "https://kauth.kakao.com/oauth/token"
 data = {
     "grant_type": "authorization_code",
     "client_id": "8e27c9db50b3a186482c7c863fc8040b",    #RESTAPI KEY
     "redirect_uri": "http://127.0.0.1:8000",
-    "code": "LSjdRSX9XogvDkJkRC7UmdszpVrYjk9UCqTvveY7cIZo4ay0kguHIQsqUTgKKwzUAAABjsDh5d22W8wW6V7rJg"
+    "code": "_ADnSeq6jmVMCA-cnWp1pvpo75SuMnBDqnAVuO-LD_OycIX-31A8EWTWoxoKPXRoAAABjuT4YqIe0jm_MNo9Pw"
 }
 
-response = requests.post(url, data= data)
-tokens = response.json()
-print(tokens)
+# 토큰 발급 코드
+#response = requests.post(url, data= data)
+#tokens = response.json()
+#print(tokens)
+
+#토큰 발급 이후 acess_token 작성
+
+access_token = "4J0_Qj3uvNkV1wzS3DRldnGrF_8mf1ltEgUKPXQRAAABjuT4mpSt1856Xp2T3g"
+msg_url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+headers = {
+    "Authorization": "Bearer " + access_token
+}
+
+msg_data = {
+    "template_object": json.dumps({
+        "object_type": "text",
+        "text": "카카오톡 테스트",
+        "link": {"mobile_web_url": "https://www.naver.com"}
+   })
+}
+response = requests.post(msg_url, headers=headers, data=msg_data)
